595567d0194e54aed6abb8f26b26574ef1796877
LukeRamsay <43437616+LukeRamsay@users.noreply.github.com>
2020-08-19 23:20:54 +0200

    Regsister and Logout
    Added the basic register and logout functionality

---
+++ app/src/main/java/com/example/easyeats/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.easyeats
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        auth = Firebase.auth
+
+        auth.addAuthStateListener { firebaseAuth ->
+            val firebaseUser = firebaseAuth.currentUser
+            if (firebaseUser == null ){
+                Log.d("Authentication", "User is not logged in")
+                findNavController().navigate(R.id.action_homeFragment_to_registerFragment)
+            }
+        }
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/com/example/easyeats/MainActivity.kt
+++ app/src/main/java/com/example/easyeats/MainActivity.kt
@@ -2,10 +2,42 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
     }
+
+     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         val inflater = menuInflater
+         inflater.inflate(R.menu.main_menu, menu)
+         return super.onCreateOptionsMenu(menu)
+     }
+
+     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         when(item.itemId) {
+             R.id.logout_btn -> {
+                 auth.signOut()
+                 Toast.makeText(this, "Logged Out", Toast.LENGTH_LONG)
+                 Log.d("Auth", "User Was Logged Out")
+                 return true
+             }
+             else -> {
+                 return super.onOptionsItemSelected(item)
+             }
+         }
+     }
 }
